b08a5aba490f69d98eff3e93fbaccb534d707178
Bison Zhang <bisonzhang@gmail.com>
2012-03-31 16:38:56 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ttgolfbe::Application.config.secret_token = '94c55ecb9fd9f940b4c035e1c2ed4471c52a74793c52e93b0adde6b9115f774bd020f09db221a49d23cbb8e6da7fbfcc3a4f0a0d1d8754ebab46bb574b37b6a0'
