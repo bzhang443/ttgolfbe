877af6e62c6c510f4c1da81f67cb62f488823fb7
Bison Zhang <bisonzhang@gmail.com>
2012-04-12 19:12:51 +0800

    hot_list removed, data merged into course_list, with client side cache in consideration

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,24 +5,12 @@ class ApiController < ApplicationController
   def index
     x = Device.first
     @token = x ? x.token : ''
-  end
-
-  def sign_up    
-    did, model, os, version, source = params[:did], params[:model], params[:os], params[:version], params[:source]
-    return render json: {:status=>1, :message=>'缺少参数'} if did.blank? || model.blank? || os.blank?  || source.blank?
-    
-    device = Device.find_or_create(did, model, os, version, source)
-    ret = {:status => 0, :token => device.token}
-    ret[:user_name] = device.user.name if device.user.name
-    
-    render json: ret
+    render :layout=>'api'
   end
 
   def sync_config
     lat_lon = params[:lat_lon]
     return render json: {:status=>1, :message=>'缺少参数'} if lat_lon.blank?
-    x, y = lat_lon.split('|')
-    logger.debug("sync_config got from (#{x},#{y})")
     
     atts = {}
     atts[:os] = params[:os] unless params[:os].blank?
@@ -34,84 +22,19 @@ class ApiController < ApplicationController
     render json: {:status => 0, :config => config}
   end
 
-  def config_user
-    atts = {}
-    u = @device.user
-    unless params[:name].blank?
-      unless u.name.eql?(params[:name])
-        return render json: {:status=>11, :message=>'用户名已存在'} if User.find_by_name params[:name]
-      end
-      atts[:name] = params[:name]
-    end
-    unless params[:email].blank?
-      unless u.email.eql?(params[:email])
-        return render json: {:status=>12, :message=>'邮箱已经注册过'} if User.find_by_email params[:email]
-      end
-      atts[:email] = params[:email]
-    end    
-    atts[:password] = params[:pass] unless params[:pass].blank?
-    atts[:sina_token] = params[:sina_token] unless params[:sina_token].blank?
-    u.update_attributes(atts)    
+  def course_list
+    return render json: {:status=>1, :message=>'缺少参数'} if params[:lat_lon].blank? && params[:area_id].blank?
     
-    ret = {:status=>0}
-    unless params[:password].blank?
-      @device.update_token
-      ret[:token] = @device.token
-    end
-    render json: ret
-  end
-
-  def switch_user
-    name, pass = params[:name], params[:pass]
-    return render json: {:status=>1, :message=>'缺少参数'} if name.blank? || pass.blank?
-    user = User.secure_get(name, pass)
-    return render json: {:status=>12, :message=>'用户验证错误'} unless user
-    
-    @device.switch_user(user)
-    
-    render json: {:status=>0, :token=>@device.token}
-  end
-
-  def find_password
-    email = params[:email]
-    return render json: {:status=>1, :message=>'缺少参数'} if email.blank?
-    him = User.find_by_email email
-    return render json: {:status=>13, :message=>'邮箱不存在'} unless him
-    
-    render json: {:status=>0, :message=>'密码已经发送，请查收'}
-  end
-  
-  def hot_list
-    lat_lon = params[:lat_lon]
-    return render json: {:status=>1, :message=>'缺少参数'} if lat_lon.blank?
-    x, y = lat_lon.split('|')
-    logger.debug("hot_list got request from (#{x},#{y})")
-    
-    list = Course.find(:all, 
-        :conditions => ["courses.id in (?)", [1,2,3]]
-    ).collect { |e| 
+    courses = Course.find(:all, :conditions => ["courses.id in (?)", [1,2,3]])
+    hot = courses.collect { |e| 
       {:id=>e.id, :name=>e.name || e.club.name, :pic=>e.images ? e.images[0].url : ''}
     }
+    list = courses.collect { |e| 
+      {:id=>e.id, :name=>e.name || e.club.name, :logo=>e.club.logo_url, :lat_lon=>"#{e.club.latitude}|#{e.club.longitude}",
+        :overall=>rand_rank, :price=>rand_rank, :hardness=>rand_rank, :view=>rand_rank}
+    }   
     
-    render json: {:status=>0, :list=>list}   
-  end
-  
-  def course_list
-    lat_lon = params[:lat_lon]
-    return render json: {:status=>1, :message=>'缺少参数'} if lat_lon.blank?
-    x, y = lat_lon.split('|')
-    logger.debug("hot_list got request from (#{x},#{y})")
-
-    by = params[:by]
-    return render json: {:status=>1, :message=>'缺少参数'} if by.blank?
-    
-    list = Course.find(:all, 
-        :conditions => ["courses.id in (?)", [1,2,3]]
-    ).collect { |e| 
-      {:id=>e.id, :name=>e.name || e.club.name, :logo=>e.club.logo_url}
-    }
-    
-    render json: {:status=>0, :list=>list}  
+    render json: {:status=>0, :version=>Time.now.to_i, :hot=>hot, :list=>list}  
   end
   
   def course_info
@@ -203,6 +126,79 @@ class ApiController < ApplicationController
     render json: {:status=>0, :list=>list}
   end
   
+  def sign_up    
+    did, model, os, version, source = params[:did], params[:model], params[:os], params[:version], params[:source]
+    return render json: {:status=>1, :message=>'缺少参数'} if did.blank? || model.blank? || os.blank?  || source.blank?
+    
+    device = Device.find_or_create(did, model, os, version, source)
+    ret = {:status => 0, :token => device.token}
+    ret[:user_name] = device.user.name if device.user.name
+    
+    render json: ret
+  end
+  
+  def config_user
+    atts = {}
+    u = @device.user
+    unless params[:name].blank?
+      unless u.name.eql?(params[:name])
+        return render json: {:status=>11, :message=>'用户名已存在'} if User.find_by_name params[:name]
+      end
+      atts[:name] = params[:name]
+    end
+    unless params[:email].blank?
+      unless u.email.eql?(params[:email])
+        return render json: {:status=>12, :message=>'邮箱已经注册过'} if User.find_by_email params[:email]
+      end
+      atts[:email] = params[:email]
+    end    
+    atts[:password] = params[:pass] unless params[:pass].blank?
+    atts[:sina_token] = params[:sina_token] unless params[:sina_token].blank?
+    u.update_attributes(atts)    
+    
+    ret = {:status=>0}
+    unless params[:password].blank?
+      @device.update_token
+      ret[:token] = @device.token
+    end
+    render json: ret
+  end
+
+  def switch_user
+    name, pass = params[:name], params[:pass]
+    return render json: {:status=>1, :message=>'缺少参数'} if name.blank? || pass.blank?
+    user = User.secure_get(name, pass)
+    return render json: {:status=>12, :message=>'用户验证错误'} unless user
+    
+    @device.switch_user(user)
+    
+    render json: {:status=>0, :token=>@device.token}
+  end
+
+  def find_password
+    email = params[:email]
+    return render json: {:status=>1, :message=>'缺少参数'} if email.blank?
+    him = User.find_by_email email
+    return render json: {:status=>13, :message=>'邮箱不存在'} unless him
+    
+    render json: {:status=>0, :message=>'密码已经发送，请查收'}
+  end  
+  
+  def hot_list
+    lat_lon = params[:lat_lon]
+    return render json: {:status=>1, :message=>'缺少参数'} if lat_lon.blank?
+    x, y = lat_lon.split('|')
+    logger.debug("hot_list got request from (#{x},#{y})")
+    
+    list = Course.find(:all, 
+        :conditions => ["courses.id in (?)", [1,2,3]]
+    ).collect { |e| 
+      {:id=>e.id, :name=>e.name || e.club.name, :pic=>e.images ? e.images[0].url : ''}
+    }
+    
+    render json: {:status=>0, :list=>list}   
+  end
+  
 private
   def check_token
     token = params[:token]
@@ -210,4 +206,8 @@ private
     @device = Device.find_by_token token
     return render json: {:status=>2, :message=>'token不存在'} unless @device
   end  
+  
+  def rand_rank
+    "#{rand(9)}.#{rand(9)}"
+  end
 end
